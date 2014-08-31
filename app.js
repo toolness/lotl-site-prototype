9ccdf842f89c62274dc3e85a7c987dad5f9df5a2
Atul Varma <varmaa@gmail.com>
2014-08-31 19:14:02 -0400

    provide our own posts.js that pulls from LOTL's wordpress JSON API.

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 var express = require('express');
+var request = require('request');
 
 var PORT = process.env.PORT || 3000;
+var BASE_API_URL = 'http://lifeofthelaw.org/api';
 
 var app = express();
+
+app.get('/posts.js', function(req, res, next) {
+  request(BASE_API_URL + '/get_recent_posts/', function(err, apiRes, body) {
+    if (err) return next(err);
+    body = JSON.parse(body);
+    body = body.posts.map(function(rawPost) {
+      var post = {
+        tags: rawPost.tags.map(function(tag) { return tag.title; }),
+        title: rawPost.title,
+        link: rawPost.url,
+        pubdate: new Date(rawPost.date).toISOString(),
+        author: rawPost.custom_fields.author
+                ? rawPost.custom_fields.author[0]
+                : rawPost.author.name,
+        excerpt: rawPost.excerpt,
+        enclosure: null,
+        thumbnail: rawPost.thumbnail_images &&
+                   rawPost.thumbnail_images['home-thumbnail']
+      };
+      return post;
+    });
+    return res
+      .type('application/javascript')
+      .send('POSTS = ' + JSON.stringify(body) + ';');
+  });
+});
 
 app.use(express.static(__dirname + '/static'));
 
