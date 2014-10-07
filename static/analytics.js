3dcc2bc249cf62a88b5272144c85ec28b9954f4b
Atul Varma <varmaa@gmail.com>
2014-10-07 13:57:41 -0400

    add google analytics to all pages, w/ DNT support.

---
+++ static/analytics.js
@@ -0,0 +1,14 @@
+if (!(window.navigator.doNotTrack == "yes" ||
+      window.navigator.doNotTrack == "1")) {
+  // This is just the tracking code Google gave us, minus the script
+  // tags, to make us comply with DRY while also slowing page views b/c of
+  // the extra request for this file (we can fix that later).
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-4923054-6', 'auto');
+  ga('send', 'pageview');
+}
